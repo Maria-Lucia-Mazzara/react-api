b81f0180420a779438fb13f8e02e5a7b68a2eebf
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-12 13:36:21 +0100

    inswerito dati nelle card devo sistemarle

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,32 @@ function App() {
   useEffect(fetchAttori, []);
   return (
     <>
+      <div className="container">
+        <div className="row">
+          <div className="card" style={{ width: '18rem' }}>
+            {attori.map((attore) => (
+              <div key={attore.id} className="card-body">
+                <h2 className="card-title">
+                  {attore.name}
+                </h2>
+                <h4 className="card-title">
+                  {attore.birth_year}
+                </h4>
+                <h4 className="card-title">
+                  {attore.nationality}
+                </h4>
+                <h4 className="card-title">
+                  {attore.biography}
+                </h4>
+                <img src={attore.image} className="card-img-top" alt="..."></img>
+                <p className="card-text">
+                  {attore.known_for}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
 
     </>
   )
